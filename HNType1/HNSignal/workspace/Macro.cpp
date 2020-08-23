e56fd7d4394675286b91b02d4d62fd2d885b868b
Jin Choi <jin.choij@cern.ch>
2020-08-23 12:00:14 +0900

    HNType 1 2017 & 2018

--- HNType1/HNSignal/workspace/Macro.cpp
+++ HNType1/HNSignal/workspace/Macro.cpp
@@ -5,12 +5,14 @@ void Macro() {
 	TString DataYear = "2016";
 	TString userflag = "RunPOGTight";
 	TString region = "preselection";
-	TString obj = "muons_tight/2";
+	TString obj = "muons_tight/1";
 	TString dist = "pt";
+	bool logy = true;
 	
 	TString pathData = "$PWD/../input/" + DataYear + "/" + userflag + "__/DATA/";
 	TString pathMC = "$PWD/../input/" + DataYear + "/" + userflag + "__/MCSamples/";
-	vector<TString> Samples{"DoubleMuon", "prompt"};
+	//vector<TString> Samples{"DoubleMuon", "prompt"};
+	vector<TString> Samples{"DoubleMuon", "VV", "tX", "others"};
 
 	FileNavi navi(pathData, pathMC, Samples);
 
@@ -29,7 +31,9 @@ void Macro() {
 
 	//==== Get histograms ====
 	TH1D* h_data = nullptr;
-	TH1D* h_prompt = nullptr;
+	TH1D* h_VV = nullptr;
+	TH1D* h_tX = nullptr;
+	TH1D* h_others = nullptr;
 	TH1D* h_fake = nullptr;
 	TH1D* h_syst = nullptr;
 	TH1D* h_comp = nullptr;
@@ -48,21 +52,35 @@ void Macro() {
 	h_fake = (TH1D*)navi.GetHist("DoubleMuon", p_fake);
 	//h_fake->Draw();
 
-	// prompt
-	TString p_prompt = region + "/" + obj + "/" + dist;
-	cout << "p_prompt: " << p_prompt << endl;
-	h_prompt = (TH1D*) navi.GetHist("prompt", p_prompt);
-	//h_prompt->Draw();
+	// tX
+	TString p_tX = region + "/" + obj + "/" + dist;
+	cout << "p_tX: " << p_tX << endl;
+	h_tX = (TH1D*) navi.GetHist("tX", p_tX);
+	//h_tX->Draw();
+
+	// VV
+	TString p_VV = region + "/" + obj + "/" + dist;
+	cout << "p_VV:" << p_VV << endl;
+	h_VV = (TH1D*) navi.GetHist("VV", p_VV);
+
+	// others
+	TString p_others = region + "/" + obj + "/" + dist;
+    cout << "p_others:" << p_others << endl;
+    h_others = (TH1D*) navi.GetHist("others", p_others);		
 	
 	// rebin
 	if (dist.Contains("pt")) {
 		h_data->Rebin(10);
-		h_prompt->Rebin(10);
+		h_tX->Rebin(10);
+		h_VV->Rebin(10);
+		h_others->Rebin(10);
 		h_fake->Rebin(10);
 	}
 	if (obj.Contains("fatjets")) {
 		h_data->Rebin(5);
-		h_prompt->Rebin(5);
+		h_tX->Rebin(5);
+		h_VV->Rebin(5);
+		h_others->Rebin(5);
 		h_fake->Rebin(5);
 	}
 	//==== set systematic for fake ====
@@ -83,8 +101,20 @@ void Macro() {
 	// Get Canvas and pad
 	TCanvas* c = new TCanvas("canvas", "", 720, 800);
 	TLegend* l = new TLegend(0.69, 0.65, 0.95, 0.92);
+	TLatex* lumi = new TLatex();
 	TPad* pUp = new TPad("pUp", "", 0, 0.25, 1, 1);
 	TPad* pDown = new TPad("pDown", "", 0, 0, 1, 0.25);
+
+	TString lumi_info;
+	if (DataYear.Contains("2016")) lumi_info = "L^{int} = 35.9 fb^{-1} (13TeV)";
+	else if (DataYear.Contains("2017")) lumi_info = "L^{int} = 41.5 fb^{-1} (13Tev)";
+	else if (DataYear.Contains("2018")) lumi_info = "L^{int} = 59.7 fb^{-1} (13TeV)";
+	else {
+		cerr << "Wrong DataYear" << endl;
+		exit(EXIT_FAILURE);
+	}
+	lumi->SetTextSize(0.04);
+	lumi->SetTextFont(42);
 
 	pUp->SetTopMargin(0.08);
     pUp->SetBottomMargin(0.02);
@@ -107,30 +137,39 @@ void Macro() {
     h_data->GetXaxis()->SetLabelSize(0);
     h_data->GetYaxis()->SetTitle(yAxisName);
 	h_data->GetYaxis()->SetRangeUser(0, y_range * 1.8);
+	if (logy) h_data->GetYaxis()->SetRangeUser(1, y_range*10);
 	//h_data->Draw("p&hist");
     
 	l->AddEntry(h_data, "Data", "lep");
 
 	// h_syst setting
     h_syst = (TH1D*)h_fake->Clone("h_syst");
-    h_syst->Add(h_prompt);
+    h_syst->Add(h_others);
+	h_syst->Add(h_tX);
+	h_syst->Add(h_VV);
 	h_syst->SetStats(0);
-    h_syst->SetFillColorAlpha(kBlack, 0.4);
+    h_syst->SetFillColorAlpha(kBlack, 0.3);
     h_syst->SetFillStyle(3144);
     l->AddEntry(h_syst, "stat+syst(fake)", "f");
-	h_syst->Draw("e2&f&same");
+	//h_syst->Draw("e2&f&same");
 
 	// h_stack setting
-	h_prompt->SetFillColorAlpha(kAzure, 0.5);
-	h_fake->SetFillColorAlpha(kRed, 0.5);
-	l->AddEntry(h_prompt, "prompt", "f");
+	h_others->SetFillColorAlpha(kAzure, 0.5);
+	h_tX->SetFillColorAlpha(kBlue, 0.5);
+	h_VV->SetFillColorAlpha(kOrange, 0.9);
+	h_fake->SetFillColorAlpha(kViolet, 0.7);
 	l->AddEntry(h_fake, "fake", "f");
+	l->AddEntry(h_VV, "VV", "f");
+	l->AddEntry(h_tX, "tX", "f");
+	l->AddEntry(h_others, "others", "f");
 	
 	h_stack = new THStack("hs", "");
-	h_stack->Add(h_prompt);
+	h_stack->Add(h_others);
+	h_stack->Add(h_tX);
+	h_stack->Add(h_VV);
 	h_stack->Add(h_fake);
-	h_stack->Draw("hist&same");
-	h_data->Draw("p&hist&same");
+	//h_stack->Draw("hist&same");
+	//h_data->Draw("p&hist&same");
 	
 	// h_comp setting
 	h_comp = (TH1D*)h_data->Clone("h_comp");
@@ -150,7 +189,7 @@ void Macro() {
     h_comp->GetXaxis()->SetTitleSize(0.1);
     h_comp->GetXaxis()->SetTitleOffset(0.8);
 
-	h_comp_syst->SetFillColorAlpha(kBlack, 0.4);
+	h_comp_syst->SetFillColorAlpha(kBlack, 0.3);
     h_comp_syst->SetFillStyle(3144);
 
 	// Draw on canvas
@@ -159,13 +198,16 @@ void Macro() {
 	h_stack->Draw("hist&same");
 	h_data->Draw("e1&same");
 	h_syst->Draw("e2&f&same");
+	if (logy) pUp->SetLogy();
 	l->Draw("same");
+	lumi->DrawLatexNDC(0.67, 0.93, lumi_info);
 
 	pDown->cd();
 	h_comp->Draw("p&hist");
 	h_comp_syst->Draw("e2&f&same");
 
 	c->cd();
+	lumi->Draw();
 	pUp->Draw();
 	pDown->Draw();
 	c->Draw();
